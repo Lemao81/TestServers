ebe3db94ba0c2e17de4eb81caa1d7ea27d9a2dfb
Reiser <juergen.reiser@neo-q.de>
2020-05-03 21:39:02 +0200

    add mocking server

---
+++ MockingServer/Controllers/MockController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MockingServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MockController : ControllerBase
+    {
+        [HttpPost("Mirth")]
+        public JsonResult MirthFake([FromBody] string[] args) => new JsonResult(args);
+    }
+}
